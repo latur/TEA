34bc5644b89779157736be4d19ff328e232e6e89
ginnyaquarius <phuongthao.12csh@gmail.com>
2016-04-14 19:17:45 +0700

    samples family

--- Client_Server/main.py
+++ Client_Server/main.py
@@ -2,6 +2,26 @@ import tornado.ioloop
 import tornado.web
 import tornado.escape
 
+def get_chip_seq(start, end):
+	chip_seq = [];
+	dis = end - start
+	f = "Bind"
+	step = 0
+
+	if (dis < 41250):
+		f += "25."
+		s
+	elif (dis < 165000) f += "100."
+	elif (dis < 1650000) f += "1000."
+	elif (dis < 16500000) f += "10000."
+	else f += "100000."
+
+	for i in range(0, 6):
+		f += i + ".bin"
+		chip_seq.append([])
+		 
+	
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):    	
     	callbackFunc = ""
@@ -19,6 +39,8 @@ class MainHandler(tornado.web.RequestHandler):
 			path = '../samples/' + name
 			content = open(path, "r")
 			ret.append(content.read())
+	elif self.request.arguments["inf"][0] == "H3K27Ac":
+		ret = get_chip_seq(self.request.arguments["start"][0], self.request.arguments["end"][0])
         self.write("{jsfunc}({json});".format(jsfunc=callbackFunc, json=tornado.escape.json_encode({"content": ret})))
         self.finish()
 
