bc531fa10cd584498442c79cf3664cba84fbc12c
ginnyaquarius <phuongthao.12csh@gmail.com>
2016-04-20 11:40:27 +0700

    filter

--- Client_Server/main.py
+++ Client_Server/main.py
@@ -108,27 +108,28 @@ def get_value(id_list):
 	return ret
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):    	
-    	callbackFunc = ""
-        if("callback" in self.request.arguments):
-            callbackFunc = self.request.arguments["callback"][0]
-            callbackFunc = str(callbackFunc)
+	def get(self):    	
+		callbackFunc = ""
+		if("callback" in self.request.arguments):
+			callbackFunc = self.request.arguments["callback"][0]
+			callbackFunc = str(callbackFunc)
 
-        self.set_header('Content-Type', 'application/javascript')
-	ret = []
-	print self.request.arguments
+		self.set_header('Content-Type', 'application/javascript')
+		ret = []
 
-	if self.request.arguments["inf"][0] == "file":
-		for name in self.request.arguments["id[]"]:
-			path = '../samples/' + name
-			content = open(path, "r")
-			ret.append(content.read())
-	elif self.request.arguments["inf"][0] == "H3K27Ac":
-		ret = get_chip_seq(int(self.request.arguments["start"][0]), int(self.request.arguments["end"][0]), int(self.request.arguments["chr"][0]))
-	elif self.request.arguments["inf"][0] == "filter":
-		ret = get_chip_seq(self.request.arguments["id_list[]"])
-        self.write("{jsfunc}({json});".format(jsfunc=callbackFunc, json=tornado.escape.json_encode(ret)))
-        self.finish()
+		if self.request.arguments["inf"][0] == "file":
+			for name in self.request.arguments["id[]"]:
+				path = '../samples/' + name
+				content = open(path, "r")
+				ret.append(content.read())
+		elif self.request.arguments["inf"][0] == "H3K27Ac":
+			ret = get_chip_seq(int(self.request.arguments["start"][0]), int(self.request.arguments["end"][0]), int(self.request.arguments["chr"][0]))
+		elif self.request.arguments["inf"][0] == "filter":
+			ret = get_chip_seq(self.request.arguments["id_list[]"])
+
+		print ret
+		self.write("{jsfunc}({json});".format(jsfunc=callbackFunc, json=tornado.escape.json_encode(ret)))
+		self.finish()
 
 def make_app():
     return tornado.web.Application([
